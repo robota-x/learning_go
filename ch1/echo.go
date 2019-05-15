81aa83f7b51599c0875b5e19543322657a16d4a6
Lorenzo Turrino <lorenzo.turrino@oddschecker.com>
2019-05-15 17:03:48 +0100

    test alternative loop

--- ch1/echo.go
+++ ch1/echo.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var s, sep string
 
 	sep = " "
-	s = "User input: "
+	s = "User input:"
 
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
+	}
+
+	fmt.Println(s)
+
+	sep = "\n"
+	s = "User input: \n"
+
+	for i, arg := range os.Args[1:] {
+		s += strconv.Itoa(i) + ")" + arg + sep
 	}
 
 	fmt.Println(s)
